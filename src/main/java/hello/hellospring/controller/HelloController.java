027eab20b2f382136acdb59b5a2ba272cbc074dc
pocachip <icanworkitout21@gmail.com>
2021-08-14 00:14:35 +0900

    스프링 웹 개발 기초 : API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    // 정적 컨텐츠
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
@@ -15,6 +17,7 @@ public class HelloController {
         return "hello";
     }
 
+    // MVC와 템플릿 엔진
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
@@ -22,4 +25,31 @@ public class HelloController {
         return "hello-template";
     }
 
+    // API
+    // @ResponseBody 문자 반환
+    @GetMapping("hello-string")
+    @ResponseBody // viewResolver를 사용하지 않고 데이터 그대로를 HTML BODY에 직접 반환(HTML body 태그가 아님)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+    // @ResponseBody 객체 반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체가가JSON으로 반환 됨
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
